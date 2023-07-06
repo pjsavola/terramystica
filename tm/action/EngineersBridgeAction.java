5ad5fff29e5bce50e43d23b8b9a861b5e13d0b44
Petri Savola <petri.j.savola@gmail.com>
2023-07-06 14:35:01 +0300

    Implement Engineer faction ability.

---
+++ tm/action/EngineersBridgeAction.java
@@ -0,0 +1,28 @@
+package tm.action;
+
+import tm.Game;
+import tm.PowerActions;
+
+public class SelectPowerActionAction extends Action {
+
+    private final int act;
+
+    public SelectPowerActionAction(int act) {
+        this.act = act;
+    }
+
+    @Override
+    public boolean canExecute() {
+        return act >= 1 && act <= 6 && !game.usedPowerActions[act - 1] && player.canAffordPower(PowerActions.getRequiredPower(act));
+    }
+
+    @Override
+    public void execute() {
+        player.usePowerAction(act);
+    }
+
+    @Override
+    public String toString() {
+        return "Action ACT" + act;
+    }
+}
